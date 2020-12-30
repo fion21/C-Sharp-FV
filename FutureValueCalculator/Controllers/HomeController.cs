9d897ec0951be4d34f9dd2c94633aef0cebb2885
Fiona Lewis <hwindepth@yahoo.com>
2020-12-30 22:48:37 +0000

    first commit

---
+++ FutureValueCalculator/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FutureValueCalculator.Models;
+
+namespace FutureValueCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FutureValue = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel fv)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FutureValue = fv.Calculate().ToString("c2");
+            }
+            else
+            {
+                ViewBag.FutureValue = "";
+            }
+
+            return View();
+            }
+
+        public IActionResult Privacy()
+        {
+         
+            return View();
+        }
+    }
+}
+
